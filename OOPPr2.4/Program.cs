b578cfc8ca0c080e567359ea45b060d3dd114421
Georgiy <larsubivec@gmail.com>
2023-02-23 18:52:48 +0400

    first commit

---
+++ OOPPr2.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    delegate int StringLengthDelegate(string s);
+
+    static void Main(string[] args)
+    {
+        List<string> strings = new List<string>() { "Ya", "Sabinina", "mama"};
+
+        StringLengthDelegate stringLengthDelegate = s => s.Length;
+
+        foreach (string s in strings)
+        {
+            int length = stringLengthDelegate(s);
+            Console.WriteLine($"The length of '{s}' is {length} symbols.");
+        }
+    }
+}
